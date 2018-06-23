28a53e4dedcd4972c100a007a5b7ae034585c07e
Torbjörn Rathsman <milasudril@gmail.com>
2018-06-23 05:01:20 +0200

    Move type name lookup functions to var
    Otherwise, there is nothing to test enumTypes against

--- var.hpp
+++ var.hpp
@@ -8,10 +8,12 @@
 #include <string>
 #include <vector>
 #include <variant>
+#include <array>
+#include <cstring>
 
 namespace DataStore
 	{
-	class Compound;
+	class Compound;  // Predeclare Compound to avoid cyclic dependency between var_t and Compound
 
 	template<class T>
 	using Array = std::vector<T>;
@@ -85,6 +87,79 @@ namespace DataStore
 		return temp->get();
 		}
 
+			template<class T>
+	constexpr decltype(T::typeName()) getTypeName() noexcept
+		{return T::typeName();}
+
+	template<class T>
+	constexpr std::enable_if_t<std::is_same_v<T, String>, char const*> getTypeName() noexcept
+		{return "str";}
+
+	template<class T>
+	constexpr std::enable_if_t<std::is_same_v<T, std::unique_ptr<String>>, char const*> getTypeName() noexcept
+		{return getTypeName<String>();}
+
+	template<class T>
+	constexpr std::enable_if_t<std::is_same_v<T, Float>, char const*> getTypeName() noexcept
+		{return "f20";}
+
+	template<class T>
+	constexpr std::enable_if_t<std::is_same_v<T, Double>, char const*> getTypeName() noexcept
+		{return "f40";}
+
+	namespace detail
+		{
+		// Helper function for concatenating two string sources into a std::array.
+		template<class SrcA, class SrcB>
+		constexpr auto strcat(SrcA const& a, SrcB const& b) noexcept
+			{
+			constexpr auto ptr_a = a();
+			constexpr auto ptr_b = b();
+			constexpr auto N_a = strlen(ptr_a);
+			constexpr auto N_b = strlen(ptr_b);
+
+			std::array<char, N_a + N_b + 1> ret{};
+			auto ptr_src = ptr_a;
+			auto ptr_dest = ret.begin();
+			while(*ptr_src)
+				{
+				*ptr_dest = *ptr_src;
+				++ptr_dest;
+				++ptr_src;
+				}
+
+			ptr_src = ptr_b;
+			while(*ptr_src)
+				{
+				*ptr_dest = *ptr_src;
+				++ptr_dest;
+				++ptr_src;
+				}
+
+			return ret;
+			}
+		}
+
+	template<class T>
+	struct ArrayTypeName
+		{
+		public:
+			static constexpr char const* typeName() noexcept
+				{return retval;}
+
+		private:
+			static constexpr auto val = detail::strcat([](){return getTypeName<T>();}
+				, [](){return "a";});
+			static constexpr auto retval = val.begin();
+		};
+
+	template<class T>
+	constexpr decltype(getTypeName<typename T::value_type>()) getTypeName() noexcept
+		{
+		using Type = typename T::value_type;
+		return ArrayTypeName<Type>::typeName();
+		}
+
 	namespace detail
 		{
 		template<class Visitor, class VariantType, int N = std::variant_size_v<VariantType>>
@@ -110,8 +185,5 @@ namespace DataStore
 		{
 		detail::VarTypeEnumerator<Visitor, VariantType>::iterate(std::forward<Visitor>(visitor));
 		}
-
-
-
 	}
 #endif
