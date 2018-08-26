4986cbcde89a398a6b18a8bd4f8260a1ab773c88
Torbjörn Rathsman <milasudril@gmail.com>
2018-08-26 19:52:42 +0200

    Import analib + Work on ValueWrapper

--- value_wrapper.hpp
+++ value_wrapper.hpp
@@ -21,20 +21,37 @@ namespace DataStore
 			ValueWrapper(ValueWrapper&&) = default;
 
 			T& get() noexcept
-				{return m_obejct;}
+				{return m_object;}
 
 			T const& get() const noexcept
 				{return m_object;}
 
 		private:
 			T m_object;
-		}
+		};
 
-	tempalte<class T>
+	template<class T>
 	class ValueWrapper<std::unique_ptr<T>>
-		{};
+		{
+		public:
+			template<class... Args>
+			ValueWrapper(Args&&... args) : m_object(std::make_unique<T>(std::forward<Args>(args)...))
+				{}
+
+			ValueWrapper(ValueWrapper const&) = default;
+			ValueWrapper& operator=(ValueWrapper const&) = default;
+			ValueWrapper& operator=(ValueWrapper&&) = default;
+			ValueWrapper(ValueWrapper&&) = default;
+
+			T& get() noexcept
+				{return *m_object;}
+
+			T const& get() const noexcept
+				{return *m_object;}
+
+		private:
+			std::unique_ptr<T> m_object;
+		};
 	};
-
-
 
 #endif
