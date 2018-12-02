f91dbb20f0b5b1b60150dcc36fe4d42b287b35f0
Torbjörn Rathsman <milasudril@gmail.com>
2018-12-02 17:27:15 +0100

    Strong TypeId type

--- key_type_count_value_defs.hpp
+++ key_type_count_value_defs.hpp
@@ -8,13 +8,56 @@
 #include <cstdint>
 #include <climits>
 
+#include <limits>
+
 namespace DataStore
 	{
 	namespace KeyTypeCountValue
 		{
 		static_assert(CHAR_BIT == 8, "This library requires 8-bit chars");
 		using KeyType = Analib::InlineString<char, 32>;
-		using TypeId = uint8_t;
+
+		class TypeId
+			{
+			public:
+				using value_type = uint8_t;
+
+				template<size_t From>
+				static constexpr TypeId create()
+					{
+					static_assert(From <= std::numeric_limits<value_type>::max());
+					TypeId ret{From};
+					return ret;
+					}
+
+				TypeId() = default;
+
+				constexpr operator size_t() const
+					{return m_value;}
+
+				constexpr bool operator==(TypeId other) const
+					{return m_value == other.m_value;}
+
+				constexpr bool operator<(TypeId other) const
+					{return m_value < other.m_value;}
+
+			private:
+				value_type m_value;
+				explicit constexpr TypeId(size_t from) : m_value(from) {}
+			};
+
+		inline constexpr bool operator!=(TypeId a, TypeId b)
+			{return !(a == b);}
+
+		inline constexpr bool operator>(TypeId a, TypeId b)
+			{return b < a;}
+
+		inline constexpr bool operator>=(TypeId a, TypeId b)
+			{return !(a < b);}
+
+		inline constexpr bool operator<=(TypeId a, TypeId b)
+			{return !(a > b);}
+
 		using ArraySize = uint32_t;
 		}
 	}
--- key_type_count_value_writer.hpp
+++ key_type_count_value_writer.hpp
@@ -20,25 +20,22 @@ namespace DataStore
 			template<class Value>
 			std::enable_if_t<std::is_arithmetic_v<Value>> operator()(KeyTypeCountValue::KeyType key, Value item)
 				{
-				r_sink.write(key)
-					.write(static_cast<KeyTypeCountValue::TypeId>(SupportedTypes::template getTypeIndex<Value>()))
-					.write(item);
+				writeHeader<Value>(key);
+				r_sink.write(item);
 				}
 
 			template<class Value>
 			std::enable_if_t<IsSimpleArray<Value>::value> operator()(KeyTypeCountValue::KeyType key, Value const& item)
 				{
-				r_sink.write(key)
-					.write(static_cast<KeyTypeCountValue::TypeId>(SupportedTypes::template getTypeIndex<Value>()))
-					.write(static_cast<KeyTypeCountValue::ArraySize>(item.size()))
+				writeHeader<Value>(key);
+				r_sink.write(static_cast<KeyTypeCountValue::ArraySize>(item.size()))
 					.write(item.data(), item.size());
 				}
 
 			void operator()(KeyTypeCountValue::KeyType key, Compound const& item)
 				{
-				r_sink.write(key)
-					.write(static_cast<KeyTypeCountValue::TypeId>(SupportedTypes::template getTypeIndex<Compound>()))
-					.write(static_cast<KeyTypeCountValue::ArraySize>(item.childCount()));
+				writeHeader<Compound>(key);
+				r_sink.write(static_cast<KeyTypeCountValue::ArraySize>(item.childCount()));
 				item.visitItems(*this);
 				}
 
@@ -46,9 +43,8 @@ namespace DataStore
 			std::enable_if_t<IsSequenceOf<Sequence<Compound>, Compound>::value>
 			operator()(KeyTypeCountValue::KeyType key, Sequence<Compound> const& item)
 				{
-				r_sink.write(key)
-					.write(static_cast<KeyTypeCountValue::TypeId>(SupportedTypes::template getTypeIndex<Sequence<Compound>>()))
-					.write(static_cast<KeyTypeCountValue::ArraySize>(item.size()));
+				writeHeader<Sequence<Compound>>(key);
+				r_sink.write(static_cast<KeyTypeCountValue::ArraySize>(item.size()));
 				std::for_each(item.begin(), item.end(), [*this](auto const& val) mutable
 					{
 					r_sink.write(static_cast<KeyTypeCountValue::ArraySize>(val.childCount()));
@@ -60,9 +56,8 @@ namespace DataStore
 			std::enable_if_t<IsSimpleArray<SimpleArray>::value && IsSequenceOf<Sequence<SimpleArray>, SimpleArray>::value>
 			operator()(KeyTypeCountValue::KeyType key, Sequence<SimpleArray> const& item)
 				{
-				r_sink.write(key)
-					.write(static_cast<KeyTypeCountValue::TypeId>(SupportedTypes::template getTypeIndex<Sequence<SimpleArray>>()))
-					.write(static_cast<KeyTypeCountValue::ArraySize>(item.size()));
+				writeHeader<Sequence<SimpleArray>>(key);
+				r_sink.write(static_cast<KeyTypeCountValue::ArraySize>(item.size()));
 				std::for_each(item.begin(), item.end(), [this](SimpleArray const& value)
 					{
 					r_sink.write(static_cast<KeyTypeCountValue::ArraySize>(value.size()))
@@ -71,6 +66,15 @@ namespace DataStore
 				}
 
 		private:
+			template<class Value>
+			void writeHeader(KeyTypeCountValue::KeyType key)
+				{
+				auto type_id = KeyTypeCountValue::TypeId::create<SupportedTypes::template getTypeIndex<Value>()>();
+
+				r_sink.write(key)
+					.write(type_id);
+				}
+
 			Writer& r_sink;
 		};
 	}
