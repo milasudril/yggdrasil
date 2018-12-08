7f865dc8bbd2857b553ee4ff2d84e77567273f2f
Torbjörn Rathsman <milasudril@gmail.com>
2018-12-08 21:16:11 +0100

    Move test data

--- key_type_count_value_deserializer.test.cpp
+++ key_type_count_value_deserializer.test.cpp
@@ -2,89 +2,19 @@
 
 #include "key_type_count_value_deserializer.hpp"
 
-#include "basic_compound.hpp"
+#include "test/compound_test.hpp"
 #include "mem_reader.hpp"
 #include "native_decoder.hpp"
 
 #include <type_traits>
 
-struct MyExceptionPolicy
-	{
-	[[noreturn]]
-	static void keyNotFound(DataStore::KeyTypeCountValueDefs::KeyType key)
-		{throw key;}
-
-	template<class T>
-	[[noreturn]]
-	static void keyValueHasWrongType(DataStore::KeyTypeCountValueDefs::KeyType key, size_t actualType)
-		{throw key;}
-
-	template<class T>
-	[[noreturn]]
-	static void keyAlreadyExists(DataStore::KeyTypeCountValueDefs::KeyType key, T const& value)
-		{throw key;}
-	};
-
-
-using Compound = DataStore::BasicCompound<MyExceptionPolicy, DataStore::KeyTypeCountValueDefs::KeyType, std::string, int>;
-
-constexpr uint8_t data_le[]=
-	{
-	 3, 0, 0, 0
-	,'A',0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,Compound::SupportedTypes::getTypeIndex<Compound>()
-	,2, 0, 0, 0
-	,'a','1', 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,Compound::SupportedTypes::getTypeIndex<int>()
-	,1, 2, 3, 4
-	,'a','2', 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,Compound::SupportedTypes::getTypeIndex<std::string>()
-	,6, 0, 0, 0
-	,'F','o','o','b','a','r'
-	,'B',0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,Compound::SupportedTypes::getTypeIndex<std::vector<Compound>>()
-	,2, 0, 0, 0
-	,1 ,0 ,0 ,0
-	,'B','0', '1', 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,Compound::SupportedTypes::getTypeIndex<int>()
-	,2, 2, 3, 4
-	,0, 0, 0, 0
-	,'C',0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,0,  0, 0, 0, 0, 0, 0, 0
-	,Compound::SupportedTypes::getTypeIndex<std::vector<std::string>>()
-	,2, 0, 0, 0
-	,5, 0, 0, 0
-	,'H','e','l','l','o'
-	,5, 0, 0, 0
-	,'W','o','r','l','d'
-	};
-
-static_assert(data_le[sizeof(DataStore::KeyTypeCountValueDefs::KeyType) + sizeof(DataStore::KeyTypeCountValueDefs::ArraySize) ]==Compound::SupportedTypes::getTypeIndex<Compound>());
-
 int main()
 	{
-	Compound compound;
+	Test::Compound compound;
 	DataStore::MemReader memReader
 		{
-		  reinterpret_cast<std::byte const*>(std::begin(data_le))
-		, reinterpret_cast<std::byte const*>(std::end(data_le))
+		  reinterpret_cast<std::byte const*>(std::begin(Test::data_le))
+		, reinterpret_cast<std::byte const*>(std::end(Test::data_le))
 		};
 
 	auto status = DataStore::KeyTypeCountValueDeserializer{DataStore::NativeDecoder{memReader}}(compound);
@@ -100,7 +30,7 @@ int main()
 		,DataStore::KeyTypeCountValueDefs::KeyType{"a2"}) == "Foobar");
 
 		{
-		auto& v = DataStore::get<std::vector<Compound>>(compound, DataStore::KeyTypeCountValueDefs::KeyType{"B"});
+		auto& v = DataStore::get<std::vector<Test::Compound>>(compound, DataStore::KeyTypeCountValueDefs::KeyType{"B"});
 		assert(v.size() == 2);
 		assert(v[0].get<int>(DataStore::KeyTypeCountValueDefs::KeyType{"B01"}) == 0x4030202);
 		assert(v[1].childCount() == 0);
---
+++ test/compound_test.hpp
@@ -0,0 +1,83 @@
+//@	{"targets":[{"name":"compound_test.hpp","type":"include"}]}
+
+#ifndef COMPOUND_TEST_H
+#define COMPOUND_TEST_H
+
+#include "basic_compound.hpp"
+#include <string>
+#include <vector>
+
+namespace Test
+	{
+	struct MyExceptionPolicy
+		{
+		[[noreturn]]
+		static void keyNotFound(DataStore::KeyTypeCountValueDefs::KeyType key)
+			{throw key;}
+
+		template<class T>
+		[[noreturn]]
+		static void keyValueHasWrongType(DataStore::KeyTypeCountValueDefs::KeyType key, size_t actualType)
+			{throw key;}
+
+		template<class T>
+		[[noreturn]]
+		static void keyAlreadyExists(DataStore::KeyTypeCountValueDefs::KeyType key, T const& value)
+			{throw key;}
+		};
+
+
+	using Compound = DataStore::BasicCompound<MyExceptionPolicy, DataStore::KeyTypeCountValueDefs::KeyType, std::string, int>;
+
+	constexpr uint8_t data_le[]=
+		{
+		3, 0, 0, 0
+		,'A',0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,Compound::SupportedTypes::getTypeIndex<Compound>()
+		,2, 0, 0, 0
+		,'a','1', 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,Compound::SupportedTypes::getTypeIndex<int>()
+		,1, 2, 3, 4
+		,'a','2', 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,Compound::SupportedTypes::getTypeIndex<std::string>()
+		,6, 0, 0, 0
+		,'F','o','o','b','a','r'
+		,'B',0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,Compound::SupportedTypes::getTypeIndex<std::vector<Compound>>()
+		,2, 0, 0, 0
+		,1 ,0 ,0 ,0
+		,'B','0', '1', 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,Compound::SupportedTypes::getTypeIndex<int>()
+		,2, 2, 3, 4
+		,0, 0, 0, 0
+		,'C',0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,0,  0, 0, 0, 0, 0, 0, 0
+		,Compound::SupportedTypes::getTypeIndex<std::vector<std::string>>()
+		,2, 0, 0, 0
+		,5, 0, 0, 0
+		,'H','e','l','l','o'
+		,5, 0, 0, 0
+		,'W','o','r','l','d'
+		};
+
+	static_assert(data_le[sizeof(DataStore::KeyTypeCountValueDefs::KeyType) + sizeof(DataStore::KeyTypeCountValueDefs::ArraySize) ]==Compound::SupportedTypes::getTypeIndex<Compound>());
+	}
+
+#endif
